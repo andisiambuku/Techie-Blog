6878ab10de676e10646554f921adc25482b4cddc
andisiambuku <roselandambuku@gmail.com>
2022-10-06 09:39:54 +0300

    feature:created the db json file and server

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
-
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
