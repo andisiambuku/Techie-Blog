cd85264429eb0fc797d0b1652ca18dcd4d5b29fa
andisiambuku <roselandambuku@gmail.com>
2022-10-08 18:31:05 +0300

    feat: added Home component

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import ArticleList from "./ArticleList";
+import useFetch from "../hooks/useFetch";
+import React from "react";
+
+function Home(){
+    const {data: articles, isPending, error} = useFetch('http://localhost:8000/articles');
+
+    return(
+        <div className="home">
+            { error && <div>{error}</div>}
+            { isPending && <div>Loading...</div>}
+            <ArticleList articles={articles} title="All Posts"/>
+        </div>
+    );
+
+}
+
+export default Home;
