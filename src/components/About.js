60caa96ea2c777f96f0ea661a50ef4abc7d9a242
andisiambuku <roselandambuku@gmail.com>
2022-10-11 09:29:37 +0300

    styling: changes to about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,8 @@ import React from "react";
 function About(){
     return(
       <div className="mt-12 lg:mt-0">
-      <h1 className="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The Tech Blog  <br /><span className="text-blue-600">centered on the digital Savannah</span></h1>
-      <p className="text-gray-600">
+      <h1 className="text-5xl text-center md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">The Tech Blog  <br /><span className="text-blue-600">centered on the digital Savannah</span></h1>
+      <p className="text-center px-10  py-8 text-gray-600">
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Eveniet, itaque accusantium odio, soluta, corrupti aliquam
         quibusdam tempora at cupiditate quis eum maiores libero
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{ useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 import ArticleList from './ArticleList';
@@ -6,6 +6,7 @@ import ArticleList from './ArticleList';
 
 function Article() {
     const { id } = useParams();
+    // const [articles,setArticles]= useState('');
     const { data: article,error,isPending } = useFetch(`http://localhost:3001/articles/${id}`);
     const navigate = useNavigate();
     console.log(id)
@@ -13,10 +14,12 @@ function Article() {
     console.log(error)
 
 
-    const handleClick = () =>{
-        fetch('http://localhost:3001/articles/' + article.id,{
+    const handleClick = async (id) =>{
+        await fetch('http://localhost:3001/articles/' + article.id,{
             method:'DELETE'
-        }).then(() => {
+        })
+        .then((res)=> res.json())
+        .then(() => {
             navigate('/');
         })
     }
@@ -26,8 +29,8 @@ function Article() {
             {error && <div>{error}</div>}
             <article>
                 <h2>{ArticleList.title}</h2>
-                <p>Written by {article.author}</p>
-                <div>{article.body}</div>
+                <p>Written by { article.author }</p>
+                <div>{ article.body }</div>
                 <button onClick={handleClick}>Delete</button>
             </article>
 
